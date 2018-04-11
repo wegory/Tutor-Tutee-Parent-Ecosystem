2c44e6aaf05848d447090268e56cb9bb89716915
gregory <gregoryteo@hotmail.sg>
2018-04-11 10:19:52 +0800

    fixed untracked files

--- app/config/constants.js
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_API_KEY = "AIzaSyDD5ghg1y_dpfFJAd-n3lrwC39jypJWnTQ";
-export const FIREBASE_AUTH_DOMAIN = "tuition-9fef5.firebaseapp.com";
-export const FIREBASE_DATABASE_URL = "https://tuition-9fef5.firebaseio.com";
-export const FIREBASE_PROJECT_ID = "tuition-9fef5";
-export const FIREBASE_STORAGE_BUCKET = "tuition-9fef5.appspot.com";
-export const FIREBASE_MESSAGING_SENDER_ID = "107154355092";
-
-export const FACEBOOK_APP_ID = "2081146318570185";
